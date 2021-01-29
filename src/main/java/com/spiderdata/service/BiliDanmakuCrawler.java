e741b74bfc0b806c01b4a8e944c84be85f4d4fbe
QASAK <w663012911@outlook.com>
2021-01-29 09:48:08 +0800

    add Date select

--- src/main/java/com/spiderdata/service/BiliDanmakuCrawler.java
+++ src/main/java/com/spiderdata/service/BiliDanmakuCrawler.java
@@ -7,6 +7,7 @@ import com.spiderdata.modules.Utils.HttpClientUtil;
 import com.spiderdata.modules.Utils.YmlUtil;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.python.antlr.ast.Str;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -27,13 +28,13 @@ import java.util.regex.Pattern;
  */
 public class BiliDanmakuCrawler {
     private static Map<String, String> map = YmlUtil.getYmlByFileName("biliconfig.yml");
-    private static String PRE = map.get("url.pre");
-    private static String DMApi = map.get("url.DMApi");
-    private static String cookies = map.get("header.cookies");
-    private static String UserAgent = map.get("header.UserAgent");
-    private static String addr = map.get("dir.addr");
-    private static String proxyIP = map.get("proxy.ip");
-    private static String proxyPort = map.get("proxy.port");
+    private static final String PRE = map.get("url.pre");
+    private static final String DM_API = map.get("url.DMApi");
+    private static final String COOKIES = map.get("header.cookies");
+    private static final String USER_AGENT = map.get("header.UserAgent");
+    private static final String ADDR = map.get("dir.addr");
+    private static final String PROXY_IP = map.get("proxy.ip");
+    private static final String PROXY_PORT = map.get("proxy.port");
 
     public String getHtmlString(String url) {
         Connection.Response resp = null;
@@ -71,12 +72,12 @@ public class BiliDanmakuCrawler {
         String tmp = htmlContent.substring(htmlContent.indexOf("\"og:title\" content=\"") + 20);
         return tmp.substring(0, tmp.indexOf('_'));
     }
-    public String[] getDanMakuURLs(String cid, String uploadDate, String endDate) throws ParseException {
-        int days = DateUtil.daysBetween(uploadDate, endDate);
+    public String[] getDanMakuURLs(String cid, String fromDate, String toDate) throws ParseException {
+        int days = DateUtil.daysBetween(fromDate, toDate);
         String[] urls = new String[days + 1];
-        String nextDay = uploadDate;
+        String nextDay = fromDate;
         for(int i = 0; i <= days; i++) {
-            String URL = DMApi + cid + "&date=" + nextDay;
+            String URL = DM_API + cid + "&date=" + nextDay;
             urls[i] = URL;
             nextDay = DateUtil.getNextDay(nextDay);
         }
@@ -86,17 +87,17 @@ public class BiliDanmakuCrawler {
     // <d p="0.13400,1,25,16777215,1442243493,0,a668adff,1210303425">我是欧洲人A路人</d>
     public static void writeContentToFile(String url, String BV, String day, String name) throws Exception{
         Map<String, String> map = new HashMap<>();
-        map.put("cookie", cookies);
-        map.put("User-Agent", UserAgent);
+        map.put("cookie", COOKIES);
+        map.put("User-Agent", USER_AGENT);
         String[] proxy = null;
-        if(proxyPort != null && proxyIP != null) {
-            proxy = new String[]{"127.0.0.1", "1080"};
+        if(PROXY_IP != null && PROXY_PORT != null) {
+            proxy = new String[]{PROXY_IP, PROXY_PORT};
         }
         String en = HttpClientUtil.doGet(url, map, proxy);
         String c = "\">(.*?)<" ;
         Pattern a = Pattern.compile(c);
         Matcher m = a.matcher(en);
-        String dir = addr + name + "_" + BV +"\\";
+        String dir = ADDR + name + "_" + BV +"\\";
         FileUtil.createDir(dir);
         File file = new File(dir + day + ".xml");
         if(file.exists()){
@@ -112,15 +113,25 @@ public class BiliDanmakuCrawler {
 //        }
         fos.write(en.getBytes());
     }
-
     public void recordDanmaku(String BV) {
+        recordDanmaku(BV, null, null);
+    }
+    public void recordDanmaku(String BV, String fromDate) {
+        recordDanmaku(BV, fromDate, null);
+    }
+    public void recordDanmaku(String BV, String fromDate, String toDate) {
         String htmlStr = getHtmlString(PRE + BV);
         String cid = getCid(BV);
-        String uploadDate = getUploadDate(htmlStr);
+        if(fromDate == null && toDate == null) {
+            fromDate = getUploadDate(htmlStr);
+            toDate = getCurDate();
+        } else if(toDate == null) {
+            toDate = getCurDate();
+        }
         String name = getName(htmlStr);
         String[] urls = null;
         try {
-            urls = getDanMakuURLs(cid, uploadDate, getCurDate());
+            urls = getDanMakuURLs(cid, fromDate, toDate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -140,9 +151,10 @@ public class BiliDanmakuCrawler {
             }
         }
     }
+
     public static void main(String[] args) {
         BiliDanmakuCrawler b = new BiliDanmakuCrawler();
         String BV = "BV1qs41117pt";
-        b.recordDanmaku(BV);
+        b.recordDanmaku(BV, "2017-10-24");
     }
 }
